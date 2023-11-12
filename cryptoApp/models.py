3d96bb39306a8163d4097cf30732e6aa25044e4c
flynn_pham <flynn_pham@Flynns-MacBook-Pro.local>
2023-11-11 20:09:38 -0500

    Table and link to detail page - reset database due to foreign key error

--- cryptoApp/models.py
+++ cryptoApp/models.py
@@ -2,8 +2,25 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser, Group, Permission
 
+
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     id_or_photo = models.FileField(upload_to='id_photos/', null=True, blank=True)
     groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
     user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+
+
+class Coin(models.Model):
+    name = models.CharField(max_length=250,null=True)
+    symbol = models.CharField(max_length=10, null=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    percentage_change_1h = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    percentage_change_24h = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    percentage_change_7d = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    market_cap = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    all_time_high = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    graph_link = models.URLField(blank=True)
+    icon_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
--- cryptoApp/urls.py
+++ cryptoApp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from cryptoApp import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.signup_view, name='signup'),
+    path('signup/', views.signup_view, name='signup'),
     path('profile/', views.profile_view, name='profile'),
     path('login/', views.custom_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.index, name='index'),
+    path('coin/<int:coin_id>', views.coin_detail, name='coin_detail')
 ]
