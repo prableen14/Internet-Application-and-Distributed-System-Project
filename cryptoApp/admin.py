8c7edda41871ffe0cbec00dbe92430f6bcd264c9
Aishwarya <johnaishwarya7@gmail.com>
2023-11-14 19:12:13 -0500

    Created models Order (temporary) and Transaction

--- cryptoApp/admin.py
+++ cryptoApp/admin.py
@@ -1,7 +1,9 @@
 # Register your models here.
 from django.contrib import admin
 from django.db import models
-from .models import CustomUser, Coin
+from .models import CustomUser, Coin, Order, Transaction
 
 admin.site.register(CustomUser)
 admin.site.register(Coin)
+admin.site.register(Order)
+admin.site.register(Transaction)
--- cryptoApp/models.py
+++ cryptoApp/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
     id_or_photo = models.FileField(upload_to='id_photos/', null=True, blank=True)
     groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
     user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=500.00)
 
 
 class Coin(models.Model):
@@ -24,3 +25,24 @@ class Coin(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+#temporary model Order. Will be replaced with actual details model
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('buy', 'Buy'),
+        ('sell', 'Sell'),
+    )
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    transaction_type = models.CharField(max_length=4, choices=TRANSACTION_TYPES)
+    balance_after_transaction = models.DecimalField(max_digits=10, decimal_places=2)
+    sold = models.BooleanField(default=False)
